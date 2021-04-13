dabae21e9792dcba721336f74863cc505a9e0e96
ahmadkharsah@hotmail.com <ahmadkharsah@hotmail.com>
2021-04-13 23:16:58 +0300

    Add project files.

---
+++ Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Microsoft.Maui;
+
+namespace mauiTest
+{
+	[Application]
+	public class MainApplication : MauiApplication<Startup>
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+			: base(handle, ownership)
+		{
+		}
+	}
+}
